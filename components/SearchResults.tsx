8ec81d7c702a29e981e2d5c6a554756323481558
Luciano Ferreira <luciano2023silva@gmail.com>
2022-03-20 17:01:41 -0300

    virtualization

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,5 +1,5 @@
-import { useMemo } from 'react';
 import ProductItem from './ProductItem';
+import { List, ListRowRenderer } from 'react-virtualized'
 
 type SearchResultsProps = {
   results: Array<{
@@ -13,18 +13,42 @@ type SearchResultsProps = {
 }
 
 export default function SearchResults({ results, onAddToWishlist, totalPrice }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem 
+          product={results[index]}
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    )
+  }
+
   return (
     <>
       <h2>Total price - {totalPrice}</h2>
-      {results.map(product => {
-      return (
-        <ProductItem 
-          key={product.title} 
-          product={product} 
-          onAddToWishlist={onAddToWishlist} 
-        />
-      );
-    })}
+
+      <List 
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+      {
+        /**
+         * {results.map(product => {
+            return (
+              <ProductItem 
+                key={product.title} 
+                product={product} 
+                onAddToWishlist={onAddToWishlist} 
+              />
+            );
+          })}
+         */
+      }
     </>
   );
 }
